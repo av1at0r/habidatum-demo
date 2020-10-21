4d7baa2ab4925355fb39d7ab92f3d6a81bdbb9ac
Nikita Livyk <livik.nick@gmail.com>
2020-10-21 19:36:26 +0300

    Updates

---
+++ precache-manifest.d4b5800f09b10c1dab19079c48e0d9b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "064da5115b758f9ea283c209f33eba8c",
+    "url": "/habidatum-demo/index.html"
+  },
+  {
+    "revision": "349fac0c6cc5eca2fbf7",
+    "url": "/habidatum-demo/static/js/2.1279719b.chunk.js"
+  },
+  {
+    "revision": "10bbee68be551dfe458d88720c58313a",
+    "url": "/habidatum-demo/static/js/2.1279719b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b1b639960f72859c6b3",
+    "url": "/habidatum-demo/static/js/main.08fd32e6.chunk.js"
+  },
+  {
+    "revision": "21082b608dcecf3803e6",
+    "url": "/habidatum-demo/static/js/runtime-main.5eac44f2.js"
+  }
+]);
